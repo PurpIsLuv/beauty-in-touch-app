2add82cba22705b37686b22eaa492ec16705d90c
purpisluv <purpisluv@gmail.com>
2021-05-22 23:16:32 +0700

    add text style

---
+++ pages/styleguide.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="styleguide">
+    <h2>Base</h2>
+    <div style="display: flex; flex-direction: column">
+      <span class="text text--bold">Выберите роль</span>
+      <span class="text text--default">Вход</span>
+      <span class="text text--primary">Клиент</span>
+      <span class="text text--secondary">Имя</span>
+      <span class="text text--placeholder">Еще нет аккаунта?</span>
+      <span class="text text--link">Зарегистрируйтесь</span>
+      <span class="text text--link text--disabled">Зарегистрируйтесь</span>
+      <span class="text text--small">
+        Нажимая кнопку Зарегистрироваться, вы принимаете Условия, Политику использования данных и Политику в отношении
+        файлов cookie. Вы можете получать от нас SMS-уведомления, отказаться от которых можете в любой момент.
+      </span>
+      <span class="text text--alert">Введите номер телефона</span>
+    </div>
+  </div>
+</template>
